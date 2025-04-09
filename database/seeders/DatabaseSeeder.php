5b55c2dff4df9c83f514add02b48181111e7a287
aj2967 <aj2967@hotmail.com>
2025-04-09 19:05:35 +0100

    Update project config

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'AJ',
+            'email' => 'aj@test.com',
+            'password' => '$2y$12$XeLzhPZ9Vu2DQpSZPDKUIOpwfVtegpz5ZZwWeodL7J9AXoiDVBUB6',
         ]);
     }
 }
