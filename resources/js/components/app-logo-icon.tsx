c37b03d6bb9998a0d8e2ec1933ca72f9d269ff71
aj2967 <aj2967@hotmail.com>
2025-04-13 00:52:09 +0100

    UI tweaks: Updated logo, theme, pointers

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -1,6 +1,11 @@
 import { SVGAttributes } from 'react';
 
+
 export default function AppLogoIcon(props: SVGAttributes<SVGElement>) {
+    return (
+        <img src="/CVLogo.svg" />
+    );
+
     return (
         <svg {...props} viewBox="0 0 40 42" xmlns="http://www.w3.org/2000/svg">
             <path
