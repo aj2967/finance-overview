c7c0f7ff576a19d93328c31c4a5392b520ff5616
aj2967 <aj2967@hotmail.com>
2025-04-12 17:55:45 +0100

    Add integrations page

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,15 @@ Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
-// Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
 //     Route::get('/', function () {
 //         return Inertia::render('dashboard');
 //     })->name('dashboard');
-// });
+
+    Route::get('/integrations', function () {
+        return Inertia::render('integrations');
+    })->name('integrations');
+});
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
