38dbb0827c53ff7b05dd770e747bdc24082bf6ca
aj2967 <aj2967@hotmail.com>
2025-04-12 17:56:22 +0100

    Create integrations list table

---
+++ database/migrations/2025_04_12_111644_create_integrations_list_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('integrations_list', function (Blueprint $table) {
+            $table->id();
+            $table->enum('status', ['active', 'inactive'])->default('inactive');
+            $table->string('name')->unique();
+            $table->string('url')->unique();
+            $table->string('description')->nullable();
+            $table->string('icon')->nullable();
+            $table->enum('category', ['general', 'bank', 'stocks', 'crypto', 'property'])->default('general');
+            $table->json('tags')->nullable()->comment('Tags or keywords for filtering or searching integrations');
+            $table->tinyInteger('is_featured')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('integrations_list');
+    }
+};
