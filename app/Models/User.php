2365ab7bb7be920c6193f4d3207973c88feb4316
aj2967 <aj2967@hotmail.com>
2025-11-17 00:34:35 +0000

    User integrations relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Integrations\UserIntegrations;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -44,5 +45,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function userIntegrations()
+    {
+        return $this->hasMany(UserIntegrations::class, 'user_id','id');
     }
 }
