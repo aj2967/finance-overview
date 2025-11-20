eb5586e3881e6f471134b22df8b59f061cbcad2e
aj2967 <aj2967@hotmail.com>
2025-11-20 20:24:34 +0000

    Bank card logos

---
+++ resources/js/components/cards/logos/index.tsx
@@ -0,0 +1,26 @@
+import AmexLogo from '@/components/cards/logos/company/amex.svg';
+import BarclaycardLogo from '@/components/cards/logos/company/barclaycard.svg';
+import HsbcLogo from '@/components/cards/logos/company/hsbc.svg';
+import MonzoLogo from '@/components/cards/logos/company/monzo.svg';
+import NationwideLogo from '@/components/cards/logos/company/nationwide.svg';
+import NatwestLogo from '@/components/cards/logos/company/natwest.svg';
+import RevolutLogo from '@/components/cards/logos/company/revolut.svg';
+import StarlingLogo from '@/components/cards/logos/company/starling.svg';
+
+import AmexNetworkLogo from '@/components/cards/logos/network/amex.svg';
+import MastercardNetworkLogo from '@/components/cards/logos/network/mastercard.svg';
+import VisaNetworkLogo from '@/components/cards/logos/network/visa.svg';
+
+export {
+    AmexLogo,
+    AmexNetworkLogo,
+    BarclaycardLogo,
+    HsbcLogo,
+    MastercardNetworkLogo,
+    MonzoLogo,
+    NationwideLogo,
+    NatwestLogo,
+    RevolutLogo,
+    StarlingLogo,
+    VisaNetworkLogo,
+};
