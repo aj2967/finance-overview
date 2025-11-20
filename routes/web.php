db1c78eb26067a817a032a849e396a06b1dc5694
aj2967 <aj2967@hotmail.com>
2025-11-20 20:31:20 +0000

    Dashboard route

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,20 @@
 <?php
 
+use App\Http\Controllers\Integration\DashboardController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 // Temporarily modify url to redirect guest users to login page
 Route::get('/home', function () {
     if (auth()->check()) {
-        return Inertia::render('Dashboard');
+        return redirect()->route('dashboard.index');
     }
 
     return Inertia::render('Welcome');
 })->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard.index');
 });
 
 require __DIR__.'/settings.php';
