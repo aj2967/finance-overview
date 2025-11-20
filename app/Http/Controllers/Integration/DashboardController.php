9f240e1b27a55b21e6d09802bcd9d400dc051f4f
aj2967 <aj2967@hotmail.com>
2025-11-20 20:21:56 +0000

    Add dashboard controller and update linked routes

---
+++ app/Http/Controllers/Integration/DashboardController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Integration;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class DashboardController extends Controller
+{
+
+    public function index(Request $request)
+    {
+        return Inertia::render('dashboard/dashboard');
+    }
+}
